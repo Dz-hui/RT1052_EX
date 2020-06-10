0319a117867b78b1d5758dbffd09e4713293bb09
sola <1225093964@qq.com>
2020-06-10 18:21:56 +0800

    加入调试函数，创建main.h

---
+++ user/main.h
@@ -0,0 +1,18 @@
+#ifndef _main_h
+#define _main_h
+
+
+
+#define SUCCESS							    1
+#define ERROR					    	    0
+
+#define DEBUG_SWITCH                        1
+#if DEBUG_SWITCH
+#define DEBUG_ERROR(fmt,arg...)             do{printf("<<-ERROR->> [%d]"fmt"\r\n",__LINE__, ##arg);}while(0)
+#define DEBUG_PRINT(fmt,arg...)             do{printf("<<-DEBUG->> [%d]"fmt"\r\n",__LINE__, ##arg);}while(0)                       
+#else
+#define DEBUG_ERROR(fmt,arg...)
+#define DEBUG_PRINT(fmt,arg...)
+#endif
+
+#endif
